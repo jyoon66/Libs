e2091315a5d5921dfe7a67cbd5a759d00b4344c9
jyoon66 <asian.tiger.juice@gmail.com>
2017-07-25 03:20:42 +0000

    finished libs

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,4 +4,17 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    Book.create(book_params)
+    redirect_to root_path
+  end
+
+  def book_params
+    params.require(:book).permit(:name, :author, :description)
+  end
+
 end
